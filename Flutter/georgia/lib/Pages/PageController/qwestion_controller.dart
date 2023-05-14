de71b0a02711c59268b9fbaa1097dd07b0b768a9
Худышка К <skinny.developer@gmail.com>
2023-05-14 17:36:43 +0400

    открытие веб вью на андроиде

--- Flutter/georgia/lib/Pages/PageController/qwestion_controller.dart
+++ Flutter/georgia/lib/Pages/PageController/qwestion_controller.dart
@@ -6,17 +6,21 @@ import 'package:georgia/Recources/enum_offsets.dart';
 import '../../Data/images_manager.dart';
 import '../../Recources/enum_font.dart';
 import '../../Support/constant.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class QwestionController extends StatelessWidget {
   final Qwestion qwestion;
 
-  QwestionController({required this.qwestion});
+  const QwestionController({super.key, required this.qwestion});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: _listAllWidgets(),
+    return Column(
+      children: _listAllWidgets(),
     );
+    // return CustomScrollView(
+    //   slivers: _listAllWidgets(),
+    // );
   }
 
   List<Widget> _listAllWidgets() {
@@ -40,6 +44,9 @@ class QwestionController extends StatelessWidget {
       list.add(spacer);
     }
 
+    list.add(_buttonWeb());
+    list.add(spacer);
+
     return list;
   }
 
@@ -55,7 +62,7 @@ class QwestionController extends StatelessWidget {
           print("object");
         },
         child: SizedBox(
-          height: Const.widthDewice,
+          height: Const.widthDewice * 0.70,
           width: Const.widthDewice,
           child: image,
         ));
@@ -123,65 +130,35 @@ class QwestionController extends StatelessWidget {
           )),
     );
   }
-}
 
-/*
-    private func generateViewButton(index: Int) -> UIView {
-        let view = UIView()
-        view.backgroundColor = EnumColors.white
-        view.addRadius(number: 8)
-        
-        let labelNumber = UILabel()
-        labelNumber.textColor = EnumColors.black
-        labelNumber.font = EnumFont.bold(18).font
-        labelNumber.text = "\(index + 1)"
-        labelNumber.textAlignment = .center
-        
-        let labelQwestion = UILabel()
-        labelQwestion.textColor = EnumColors.black
-        labelQwestion.font = EnumFont.semibold(18).font
-        labelQwestion.textAlignment = .left
-        labelQwestion.numberOfLines = 0
-        labelQwestion.text = qwestion?.answer.qwestions[index]
-        
-        view.addSubview(labelNumber)
-        labelNumber.snp.makeConstraints { make in
-            make.size.equalTo(20)
-            make.left.equalToSuperview().inset(Offsets.constant8)
-            make.centerY.equalTo(view.snp.centerY)
-        }
-        
-        let widthLabel = wDdevice - 20 - 2 * Offsets.constant8 - 2 * Offsets.constant16
-        view.addSubview(labelQwestion)
-        labelQwestion.snp.makeConstraints { make in
-            make.right.top.bottom.equalToSuperview().inset(Offsets.constant8)
-            make.left.equalTo(labelNumber.snp.right).offset(Offsets.constant8)
-            make.height.greaterThanOrEqualTo(20)
-            make.width.equalTo(widthLabel)
-        }
-        
-        view.addBorder(number: 3, color: EnumColors.black)
-        
-        if let answerTest = qwestion?.answerTest {
-            if answerTest.indexTrue == index + 1 {
-                view.backgroundColor = EnumColors.green
-            } else if answerTest.indexWrong == index + 1 {
-                view.backgroundColor = EnumColors.red
-            }
-        } else {
-            
-            let button = UIButton()
-            button.tag = index + 1
-            button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-            button.setTitle(nil, for: .normal)
-            view.addSubview(button)
-            button.snp.makeConstraints { make in
-                make.top.bottom.left.right.equalToSuperview()
-            }
-        }
-        
-        
-        
-        return view
-    }
-*/
+  Widget _buttonWeb() {
+    return Container(
+      width: double.infinity,
+      child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: EnumOffsets.offset16.offset()),
+          child: MaterialButton(
+              onPressed: () async {
+                const url =
+                    "http://teoria.on.ge/tickets?ticket=${qwestion.idQwestion}";
+                final uri = Uri.parse(url);
+                if (await canLaunchUrl(uri)) {
+                  await launchUrl(uri);
+                }
+              },
+              color: EnumColors.black.color(),
+              height: 40,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Text(
+                "На сайт (${qwestion.idQwestion})",
+                textAlign: TextAlign.center,
+                style: TextStyleExtension.generate(
+                    size: 18,
+                    style: EnumFontStyle.semiBold,
+                    color: EnumColors.white),
+              ))),
+    );
+  }
+}
